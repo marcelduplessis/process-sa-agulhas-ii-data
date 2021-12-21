14820041ceb8be0efa47666c5fd2e699993777de
marcel_duplessis <marceldpl10@gmail.com>
2021-12-21 16:16:21 +0200

    update csv_to_mat

--- sds/csv_to_mat.py
+++ sds/csv_to_mat.py
@@ -2,9 +2,9 @@ import scipy.io as sio
 import pandas as pd
 import os
 
-fname = 'aws/aws_20211215_20211220'
-# fname = 'SDS_Export_161221_094610'
+filePath='sds_test_data/'
+file = 'SDS_Export_161221_094610'
 
-df=pd.read_csv(fname+'.csv')
+df=pd.read_csv(filePath+file+'.csv')
 mat_data = {name: col.values for name, col in df.items()}
-sio.savemat(os.path.join(fname+'.mat'), mat_data)
+sio.savemat(os.path.join(filePath+file+'.mat'), mat_data)
